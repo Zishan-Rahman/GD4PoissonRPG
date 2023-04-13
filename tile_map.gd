b92e26d8d25e3a56e14219a79c4462b5c9540a70
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-13 18:30:21 +0100

    Add player character and tile arrays and values

--- tile_map.gd
+++ tile_map.gd
@@ -1,12 +1,59 @@
 extends TileMap
 
-var cell_points: Array[Vector2]
-@export var point_radius: float = 1.0
-@export var region_size: Vector2 = Vector2.ONE
-@export var rejection_samples: int = 30
+var buildings: Array[Vector2i] = [
+	Vector2i(0, 19),
+	Vector2i(1, 19),
+	Vector2i(2, 19),
+	Vector2i(3, 19),
+	Vector2i(4, 19),
+	Vector2i(5, 19),
+	Vector2i(6, 19),
+	Vector2i(7, 19),
+	Vector2i(8, 20),
+	Vector2i(0, 20),
+	Vector2i(1, 20),
+	Vector2i(2, 20),
+	Vector2i(3, 20),
+	Vector2i(4, 20),
+	Vector2i(5, 20),
+	Vector2i(6, 20),
+	Vector2i(7, 20),
+	Vector2i(8, 20),
+	Vector2i(0, 21),
+	Vector2i(1, 21),
+	Vector2i(2, 21),
+	Vector2i(3, 21),
+	Vector2i(4, 21),
+	Vector2i(5, 21),
+	Vector2i(6, 21),
+	Vector2i(7, 21),
+	Vector2i(8, 21)
+]
+var trees: Array[Vector2i] = [
+	Vector2i(0,1),
+	Vector2i(1,1),
+	Vector2i(2,1),
+	Vector2i(3,1),
+	Vector2i(4,1),
+	Vector2i(5,1),
+	Vector2i(6,1),
+	Vector2i(7,1),
+	Vector2i(0,2),
+	Vector2i(1,2),
+	Vector2i(2,2),
+	Vector2i(3,2),
+	Vector2i(4,2)
+]
+const PLAYER_SPRITE: Vector2i = Vector2i(24, 7)
+var player_placement_cell: Vector2i
 
 var x_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_width") / tile_set.tile_size.x
 var y_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_height") / tile_set.tile_size.y
+
+var cell_points: Array[Vector2]
+@export var point_radius: float = 1.0
+@export var region_size: Vector2 = Vector2(x_tile_range, y_tile_range)
+@export var rejection_samples: int = 30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +62,31 @@ func _ready():
 	cell_points = generate_points(point_radius, region_size, rejection_samples)
 	var new_time: float = Time.get_ticks_msec() - start_time
 	print("Time taken: " + str(new_time) + "ms")
+	place_player()
+
+func _get_player_placement_cell() -> Vector2i:
+	return Vector2i(randi() % x_tile_range, randi() % y_tile_range)
+
+func place_player():
+	while get_used_cells(0).has(player_placement_cell):
+		player_placement_cell = _get_player_placement_cell()
+	set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
+
+func _is_not_out_of_bounds(cell: Vector2i) -> bool:
+	return cell.x >= 0 and cell.x < x_tile_range and cell.y >= 0 and cell.y < y_tile_range
+
+func _physics_process(_delta):
+	var previous_cell: Vector2i = player_placement_cell
+	var direction: Vector2i = Vector2i.ZERO
+	if Input.is_action_pressed("ui_up"): direction = Vector2i.UP
+	elif Input.is_action_pressed("ui_down"): direction = Vector2i.DOWN
+	elif Input.is_action_pressed("ui_left"): direction = Vector2i.LEFT
+	elif Input.is_action_pressed("ui_right"): direction = Vector2i.RIGHT
+	var new_placement_cell: Vector2i = player_placement_cell + direction
+	if (not get_used_cells(0).has(new_placement_cell) or trees.has(get_cell_atlas_coords(0, new_placement_cell))) and _is_not_out_of_bounds(new_placement_cell):
+		player_placement_cell = new_placement_cell
+		set_cell(0, previous_cell, 0)
+		set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
 
 func generate_points(radius: float, sample_region_size: Vector2, number_of_samples_before_rejection: int = 30) -> Array[Vector2]:
 	var cell_size: float = radius / sqrt(2)
